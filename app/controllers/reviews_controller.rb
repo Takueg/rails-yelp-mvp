e60e2bf2f2fbc30c4394730e798b5909fdad3d18
Takudzwa Gwindingwi <takueg@gmail.com>
2021-07-14 09:16:04 +0200

    adding reviews feature

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,27 @@
 class ReviewsController < ApplicationController
-  before_action :find_restaurant, only: [ :new ]
+  before_action :find_restaurant
+  # , only: [ :new ]
 
   def new
     @review = Review.new
     @review.restaurant = @restaurant
   end
 
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant_id = @restaurant.id
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
   private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 
   def find_restaurant
     @restaurant = Restaurant.find(params[:restaurant_id])
